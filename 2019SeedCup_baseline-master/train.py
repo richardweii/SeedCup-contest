d0ab1ccc075374b766d3c7f47bee75b0f3907516
Cefixime <1627889449@qq.com>
2019-10-10 17:47:18 +0800

    adding files

---
+++ 2019SeedCup_baseline-master/train.py
@@ -0,0 +1,134 @@
+import torch
+import os
+from config import Config
+from model import My_MSE_loss
+import torch.backends.cudnn as cudnn
+from dataLoader import TrainSet, ValSet
+from model import Network
+from evaluation import calculateAllMetrics
+import datetime
+from dateutil.relativedelta import relativedelta
+from tqdm import tqdm
+
+opt = Config()
+
+# prepare dataset
+print("==> loading data...")
+trainset = TrainSet(opt.TRAIN_FILE, opt=opt)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=opt.TRAIN_BATCH_SIZE, shuffle=True)
+
+valset = ValSet(opt.VAL_FILE, opt=opt)
+valloader = torch.utils.data.DataLoader(valset, batch_size=opt.VAL_BATCH_SIZE, shuffle=False)
+print("==> load data successfully")
+# load model
+# if os.path.exists(opt.MODEL_SAVE_PATH):
+#     net = torch.load(opt.MODEL_SAVE_PATH)
+#     print("==> load model successfully")
+# else:
+#     print("==> model file dose not exist : ", opt.MODEL_SAVE_PATH)
+# setup network
+net = Network(opt)
+if opt.USE_CUDA:
+    print("==> using CUDA")
+    net = torch.nn.DataParallel(
+        net, device_ids=range(torch.cuda.device_count())).cuda()
+    cudnn.benchmark = True
+# set criterion (loss function)
+criterion_1 = torch.nn.MSELoss()
+criterion_2 = My_MSE_loss()
+
+# you can choose metric in [accuracy, MSE, RankScore]
+highest_metrics = 100
+lower_percent = 0.975
+
+
+def train(epoch):
+    net.train()
+    print("train epoch:", epoch)
+    optimizer = torch.optim.Adam(net.parameters(), lr=opt.get_lr(epoch))
+    for batch_idx, (inputs, targets_sign_day, targets_sign_hour) in enumerate(tqdm(trainloader)):
+        if opt.USE_CUDA:
+            inputs = inputs.cuda()
+            targets_sign_day = targets_sign_day.cuda()
+            targets_sign_hour = targets_sign_hour.cuda()
+
+        inputs = torch.autograd.Variable(inputs)
+        targets_sign_day = torch.autograd.Variable(targets_sign_day.float())
+        targets_sign_hour = torch.autograd.Variable(targets_sign_hour.float())
+
+        optimizer.zero_grad()
+
+        (output_FC_1_1, output_FC_1_2) = net(inputs.float())
+
+        output_FC_1_1 = output_FC_1_1.reshape(-1)
+
+        output_FC_1_2 = output_FC_1_2.reshape(-1)
+
+        loss_1_1 = criterion_2(output_FC_1_1, targets_sign_day)
+
+        loss_1_2 = criterion_1(output_FC_1_2, targets_sign_hour)
+
+        loss_day = loss_1_1
+        loss_hour = loss_1_2
+
+        loss = loss_day + loss_hour
+        loss.backward()
+
+        optimizer.step()
+
+        # TODO add to tensorboard
+    print("==> epoch {}: loss_day is {}, loss_hour is {} ".format(epoch, loss_day, loss_hour))
+
+
+def val(epoch):
+    global highest_metrics
+    net.eval()
+    pred_signed_time = []
+    real_signed_time = []
+    for batch_idx, (inputs, payed_time, signed_time) in enumerate(tqdm(valloader)):
+        if opt.USE_CUDA:
+            inputs = inputs.cuda()
+
+        inputs = torch.autograd.Variable(inputs)
+        (output_FC_1_1, output_FC_1_2) = net(inputs.float())
+
+        # calculate pred_signed_time via output
+        for i in range(len(inputs)):
+            pred_time_day = output_FC_1_1[i]
+            pred_time_hour = output_FC_1_2[i]
+            # if output_FC_1_2[i] < 10:
+            #     pred_time_hour = 10
+            # elif output_FC_1_2[i] > 19:
+            #     pred_time_hour = 19
+            temp_payed_time = payed_time[i]
+            temp_payed_time = datetime.datetime.strptime(temp_payed_time, "%Y-%m-%d %H:%M:%S")
+            temp_payed_time = temp_payed_time.replace(hour=int(pred_time_hour))
+
+            temp_pred_signed_time = temp_payed_time + relativedelta(days=int(pred_time_day))
+            temp_pred_signed_time = temp_pred_signed_time.replace(hour=int(pred_time_hour))
+            temp_pred_signed_time = temp_pred_signed_time.replace(minute=0)
+            temp_pred_signed_time = temp_pred_signed_time.replace(second=0)
+
+            pred_signed_time.append(temp_pred_signed_time.strftime("%Y-%m-%d %H"))
+            real_signed_time.append(signed_time[i])
+
+    (rankScore_result, onTimePercent_result, accuracy_result) = calculateAllMetrics(real_signed_time, pred_signed_time)
+    print("==> epoch {}: rankScore is {}, onTimePercent is {}, accuracy is {}".format(epoch, rankScore_result,
+                                                                                      onTimePercent_result,
+                                                                                      accuracy_result))
+
+    # save model
+    if rankScore_result < highest_metrics and onTimePercent_result > lower_percent:
+        print("==> saving model")
+        print("==> onTimePercent {} | rankScore {} ".format(onTimePercent_result, rankScore_result))
+        highest_metrics = rankScore_result
+        torch.save(net, opt.MODEL_SAVE_PATH)
+
+
+# start training
+if __name__ == '__main__':
+    for i in range(opt.NUM_EPOCHS):
+        train(i)
+
+        if i % opt.val_step == 0:
+            val(i)
---
+++ Preliminary_contest/dataloader.py
@@ -0,0 +1,209 @@
+import csv
+import numpy as np
+import datetime
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+'''
+Dataset information
+0 uid
+1 plat_form
+2 biz_type
+3 create_time
+4 payed_time
+
+5 product_id
+6 cate1_id
+7 cate2_id
+8 cate3_id
+9 preselling_shipped_time
+10 seller_uid
+
+11 company_name
+12 *lgst_company
+13 *warehouse_id
+14 *shipped_prov_id
+15 *shipped_city_id
+
+16 rvcr_prov_name
+17 rvcr_city_name
+18 *shipped_time
+19 *got_time
+20 *dlved_time
+21 *signed_time
+'''
+
+
+class TrainSet(Dataset):
+    def __init__(self, source_file, opt=None):
+
+        with open(source_file, 'r') as f:
+            i_range = int((len(f.readlines()) - 1) * opt.Train_Val_ratio)
+            self.len = i_range
+
+        with open(source_file, 'r') as f:
+            reader = csv.reader(f)
+            header_row = next(reader)[0]
+            self.inputs = []
+            self.targets_sign_day = []
+            self.targets_sign_hour = []
+
+            for i in tqdm(range(i_range)):
+                data = next(reader)[0].split('\t')
+
+                temp_input = [float(data[1]),
+                              float(data[2]), float(data[5]),
+                              float(data[6]), float(data[7]),
+                              float(data[8]), float(data[10]),
+                              float(data[11]), float(data[16]),
+                              float(data[17])]
+                temp_input = np.array(temp_input)
+
+                # calculate time between payed_time and signed_time
+
+                try:
+                    temp_payed_time = datetime.datetime.strptime(data[4], "%Y-%m-%d %H:%M:%S")
+                    temp_shipped_time = datetime.datetime.strptime(data[18], "%Y-%m-%d %H:%M:%S")
+                    temp_got_time = datetime.datetime.strptime(data[19], "%Y-%m-%d %H:%M:%S")
+                    temp_dlved_time = datetime.datetime.strptime(data[20], "%Y-%m-%d %H:%M:%S")
+                    temp_signed_time = datetime.datetime.strptime(data[21], "%Y-%m-%d %H:%M:%S")
+                except Exception as e:
+                    continue
+
+                time_interval_1 = temp_signed_time.day - temp_payed_time.day
+                time_interval_2 = temp_signed_time.day - temp_shipped_time.day
+                time_interval_3 = temp_signed_time.day - temp_got_time.day
+                time_interval_4 = temp_signed_time.day - temp_dlved_time.day
+
+                # remove noise
+                if time_interval_1 <= 0 or time_interval_2 <= 0 or time_interval_3 <= 0 or time_interval_4 <= 0:
+                    continue
+                if time_interval_1 >= 20:
+                    continue
+                temp_input = np.append(temp_payed_time.hour, temp_input)
+                self.inputs.append(temp_input)
+
+                self.targets_sign_day.append(time_interval_1)
+                self.targets_sign_hour.append(temp_signed_time.hour)
+        print("==> in TrainSet, len of inputs is ",len(self.inputs))
+        print("==> in TrainSet, inputs.shape  is ", np.shape(self.inputs))
+
+    def __getitem__(self, idx):
+        inputs = self.inputs[idx]
+        targets_sign_day = self.targets_sign_day[idx]
+        targets_sign_hour = self.targets_sign_hour[idx]
+        return inputs, targets_sign_day, targets_sign_hour
+
+    def __len__(self):
+        return len(self.inputs)
+
+
+class ValSet(Dataset):
+    def __init__(self, source_file, opt=None):
+
+        with open(source_file, 'r') as f:
+            i_range = len(f.readlines()) - 1
+            self.len = i_range
+
+        with open(source_file, 'r') as f:
+            reader = csv.reader(f)
+            header_row = next(reader)[0]
+            self.inputs = []
+            self.payed_time = []
+            self.signed_time = []
+
+            with tqdm(total=i_range - int(i_range * opt.Train_Val_ratio)) as tq:
+                for i in range(i_range):
+                    # avoid having same data with TrainSet
+                    data = next(reader)[0].split('\t')
+                    if i < int(i_range * opt.Train_Val_ratio):
+                        continue
+                    temp_input = [float(data[1]),
+                                  float(data[2]), float(data[5]),
+                                  float(data[6]), float(data[7]),
+                                  float(data[8]), float(data[10]),
+                                  float(data[11]), float(data[16]),
+                                  float(data[17])]
+                    try:
+                        temp_payed_time = datetime.datetime.strptime(data[4], "%Y-%m-%d %H:%M:%S")
+                    except Exception as e:
+                        print("[Error!]")
+                    temp_input = np.append(temp_payed_time.hour, temp_input)
+
+                    self.inputs.append(temp_input)
+                    self.payed_time.append(data[4])
+                    self.signed_time.append(data[21])
+                    tq.update(1)
+            print("==> in ValSet, len(inputs)   is ", len(self.inputs))
+            print("==> in ValSet, inputs.shape   is ", np.shape(self.inputs))
+
+    def __getitem__(self, idx):
+        inputs = self.inputs[idx]
+        payed_time = self.payed_time[idx]
+        signed_time = self.signed_time[idx]
+        return (inputs, payed_time, signed_time)
+
+    def __len__(self):
+        return len(self.inputs)
+
+
+'''
+available data fields in Testset
+0 uid	
+1 plat_form	
+2 biz_type	
+3 create_time	
+4 payed_time	
+5 product_id	
+6 cate1_id	
+7 cate2_id	
+8 cate3_id	
+9 preselling_shipped_time	
+10 seller_uid	
+11 company_name	
+12 rvcr_prov_name	
+13 rvcr_city_name
+'''
+
+
+class TestSet(Dataset):
+    def __init__(self, source_file, opt=None):
+        with open(source_file, 'r') as f:
+            i_range = len(f.readlines()) - 1
+            self.len = i_range
+
+        with open(source_file, 'r') as f:
+            reader = csv.reader(f)
+            header_row = next(reader)[0]
+
+            self.inputs = []
+            self.payed_time = []
+
+            for i in range(i_range):
+                data = next(reader)[0].split('\t')
+                try:
+                    temp_payed_time = datetime.datetime.strptime(data[4], "%Y-%m-%d %H:%M:%S")
+                except Exception as e:
+                    print("[Error!]")
+                temp_input = [float(data[1]),
+                              float(data[2]), float(data[5]),
+                              float(data[6]), float(data[7]),
+                              float(data[8]), float(data[10]),
+                              float(data[11]), float(data[12]),
+                              float(data[13])]
+                temp_input = np.append(temp_payed_time.hour, temp_input)
+                temp_input = np.array(temp_input)
+
+                self.inputs.append(temp_input)
+                self.payed_time.append(data[4])
+
+            print("==> in TestSet, len(inputs)   is ", len(self.inputs))
+            print("==> in TestSet, inputs.shape   is ", np.shape(self.inputs))
+
+    def __getitem__(self, idx):
+        inputs = self.inputs[idx]
+        payed_time = self.payed_time[idx]
+        return inputs, payed_time
+
+    def __len__(self):
+        return len(self.inputs)
